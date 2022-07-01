94a99e922e2365b7ac5415b7eb3980afabd20ca9
Thomas <thomas.pellan@gmail.com>
2022-07-01 17:53:09 +0200

    feat(interval) checking health at an interval

--- components/HealthStatusTrigger.vue
+++ components/HealthStatusTrigger.vue
@@ -1,23 +1,48 @@
 <template>
   <v-col>
-    <v-btn
-      :loading="isLoading"
-      @click="checkHealth">
-      Check Health !
-    </v-btn>
+    <v-row>
+      <v-btn
+        class="button-check"
+        :loading="isLoading"
+        @click="checkHealth">
+        Check Health !
+      </v-btn>
+      <v-checkbox
+        v-model="continuous"
+        :label="`Check every ${interval} milliseconds`"
+      />
+    </v-row>
+    <v-row>
+      <p v-if="lastCheckDate">
+        Last check date : {{ lastCheckDate }}
+      </p>
+    </v-row>
   </v-col>
 </template>
 
 <script>
 import { mapActions, mapMutations } from 'vuex'
+import moment from 'moment'
+
 import { BACKEND_STATUS } from '@/utils/enums'
 
 export default {
   name: 'HealthStatusTrigger',
   data () {
     return {
-      isLoading: false
+      isLoading: false,
+      continuous: true,
+      checkInterval: null,
+      interval: 5000,
+      lastCheckDate: null
     }
+  },
+  created () {
+    this.checkInterval = setInterval(() => {
+      if (this.continuous) {
+        this.checkHealth()
+      }
+    }, this.interval)
   },
   methods: {
     ...mapActions({
@@ -31,7 +56,17 @@ export default {
       this.setHealth(BACKEND_STATUS.UNKOWN)
       await this.getHealth()
       this.isLoading = false
+      this.lastCheckDate = moment(new Date()).format('MM/DD/YYYY hh:mm:ss')
     }
+  },
+  beforeDestroy () {
+    clearInterval(this.checkInterval)
   }
 }
 </script>
+
+<style>
+  .button-check {
+    margin: 1rem;
+  }
+</style>
