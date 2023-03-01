0bc414c3fe6aa9773f761b729699cf4e25b2b323
thomas <thomas.pellan@gmail.com>
2023-03-01 23:15:27 +0100

    feat(nuxt bridge) playing with nuxt bridge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
-export default {
+import {defineNuxtConfig} from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - scheduler-front',
@@ -9,35 +11,29 @@ export default {
       lang: 'en'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
+  css: [],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/api.js' }
+    {src: '@/plugins/api.js'}
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify',
     'moment'
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
@@ -49,17 +45,12 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/'
   },
-  publicRuntimeConfig: {
-    axios: {
-      browserBaseURL: process.env.SCHEDULER_API_URL
+  runtimeConfig: {
+    baseURL: process.env.SCHEDULER_API_URL,
+    public: {
+      browserBaseURL: process.env.SCHEDULER_API_URL,
     }
   },
-  privateRuntimeConfig: {
-    axios: {
-      baseURL: process.env.SCHEDULER_API_URL
-    }
-  },
-
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
@@ -78,8 +69,8 @@ export default {
       }
     }
   },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['iron-webcrypto', 'unhead'],
   }
-}
+})
